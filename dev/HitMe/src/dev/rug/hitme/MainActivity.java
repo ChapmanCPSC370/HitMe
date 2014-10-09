76005a453e15469f910208ebf45d72941ba1e187
Doug Woodward <woodw116@mail.chapman.edu>
2014-10-08 22:00:06 -0700

    App now capable of location finding

--- dev/HitMe/src/dev/rug/hitme/MainActivity.java
+++ dev/HitMe/src/dev/rug/hitme/MainActivity.java
@@ -1,35 +1,64 @@
 package dev.rug.hitme;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
 
 	// Global constants
     /*
      * Define a request code to send to Google Play services
      * This code is returned in Activity.onActivityResult
      */
-    private final static int
-            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-	
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	LocationClient mLocationClient;
+	Location mCurrentLocation;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		/*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(this, this, this);
+		
+		
 		setContentView(R.layout.activity_main);
 	}
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+		mLocationClient.connect();
+	}
 
+	@Override
+	protected void onStop(){
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -48,6 +77,66 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	public void getLocation(View v){
+		mCurrentLocation = mLocationClient.getLastLocation();
+		TextView tv = (TextView)findViewById(R.id.Location);
+		tv.setText(Double.toString(mCurrentLocation.getLatitude()));
+	}
+	
+	@Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+
+    }
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+           // showAlertDialog(connectionResult.getErrorCode());
+        }
+    }
+	
+	
+	
+	
 	
 	// Define a DialogFragment that displays the error dialog
     public static class ErrorDialogFragment extends DialogFragment {
@@ -124,8 +213,5 @@ public class MainActivity extends ActionBarActivity {
         }
     }
     
-    public void locate(){
-    	
-    }
 }
 
