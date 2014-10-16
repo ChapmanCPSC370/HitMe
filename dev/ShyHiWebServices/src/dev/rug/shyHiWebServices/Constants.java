c5d738628149e65fdb614b8017bc95b0aae44528
Doug Woodward <woodw116@mail.chapman.edu>
2014-10-15 23:36:23 -0700

    Begun Development on web services

---
+++ dev/ShyHiWebServices/src/dev/rug/shyHiWebServices/Constants.java
@@ -0,0 +1,10 @@
+package dev.rug.shyHiWebServices;
+
+//Change these parameters according to your DB
+public class Constants {
+  public static String dbClass = "com.mysql.jdbc.Driver";
+  private static String dbName= "ShyHi";
+  public static String dbUrl = "jdbc:mysql://104.131.63.149/:3306/"+dbName;
+  public static String dbUser = "root";
+  public static String dbPwd = "1eXuraux";
+}
